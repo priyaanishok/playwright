a3cda26d322351af3202da8ed60349c97a57b0ba
priyaanishok <iyer.priyaa@gmail.com>
2026-01-12 22:39:20 -0400

    Update Week1-Assignment

--- Week1-Assignment/04-UnderstandingScopingInJavascript.js
+++ Week1-Assignment/04-UnderstandingScopingInJavascript.js
@@ -9,11 +9,17 @@ function printGender(){
         console.log("Inside the if block , the colour is "+ colour);
     }
     console.log("Inside the function, the age is "+ age);
-    
+    console.log("Inside the function, but outside the if condition the colour is "+ colour);
 }
 
-console.log("The gender is "+ genderType);
-printGender();
+console.log("The gender is "+ genderType); //Prints female
+printGender(); 
 var genderType = 'male';
 printGender();
 console.log("The gender is "+ genderType);
+
+/* The gender is female
+Inside the if block , the colour is pink
+Inside the function, the age is 30
+Inside the function, the age is undefined
+The gender is male */
